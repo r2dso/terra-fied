0d1ef8ce675d452d155c84f1c2ab6306b95c1dd2
Ken Toler <kentoler@gmail.com>
2023-07-25 13:44:49 -0400

    added comments to network module

--- basic_terraform/network/main-lab.tf
+++ basic_terraform/network/main-lab.tf
@@ -1,29 +1,39 @@
+# Creates a VPC with a public subnet, internet gateway, route table, and security group.
+
+# Define the VPC with a standard CIDR block
 resource "aws_vpc" "r2dso-lab_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
+# Define the subnet with a standard CIDR block within the VPC
 resource "aws_subnet" "r2dso-lab_subnet" {
   vpc_id = aws_vpc.r2dso-lab_vpc.id
   cidr_block = "10.0.1.0/24"
 }
 
+# Define the Internet Gateway using the VPC ID
 resource "aws_internet_gateway" "r2dso-lab_igw" {
   vpc_id = aws_vpc.r2dso-lab_vpc.id
 }
 
+# Create a route table for the VPC
 resource "aws_route_table" "r2dso-lab_route_table" {
   vpc_id = aws_vpc.r2dso-lab_vpc.id
 
+  #Create a route for the Internet Gateway going to the public internet
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.r2dso-lab_igw.id
   }
 }
 
+# Associate the subnet with the route table
 resource "aws_route_table_association" "r2dso-lab_subnet_association" {
   subnet_id = aws_subnet.r2dso-lab_subnet.id
   route_table_id = aws_route_table.r2dso-lab_route_table.id
 }
+
+# Create a security group for the VPC that allows SSH and HTTP traffic
 resource "aws_security_group" "r2dso-lab_sg" {
   name_prefix = "r2dso-lab_sg"
   vpc_id = aws_vpc.r2dso-lab_vpc.id
@@ -50,6 +60,7 @@ resource "aws_security_group" "r2dso-lab_sg" {
   }
 }
 
+# Ouput the security group and subnet IDs to be used by the EC2 instance
 output "r2dso-lab-sg" {
   value = aws_security_group.r2dso-lab_sg.id
 }
