7bb383f2a6498f5eb5103f65e4505d6ffff9e0f8
Ken Toler <kentoler@gmail.com>
2023-10-09 20:29:54 -0400

    fixed vars example

--- basic_terraform/vars.tf
+++ basic_terraform/vars.tf
@@ -8,28 +8,3 @@ variable "remoteexec_enabled" {
 variable "weak_enabled" {
     type = bool
 }
-
-provider "aws" {
-  region = "us-west-2"
-}
-
-data "aws_ami" "latest_amazon_linux" {
-  most_recent = true
-
-  owners = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-output "ami_id" {
-  description = "The ID of the latest Amazon Linux 2 AMI"
-  value       = data.aws_ami.latest_amazon_linux.id
-}
